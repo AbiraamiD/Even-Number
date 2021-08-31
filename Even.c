adf8bc1189546eee44b0e92a173d469719bd4422
AbiraamiD <74908902+AbiraamiD@users.noreply.github.com>
2021-08-31 08:46:39 +0530

    Update Even.c

--- Even.c
+++ Even.c
@@ -4,4 +4,10 @@ int main()
   int i,n;
   printf("Print all the even numbers till:");
   scanf("%d,&n);
-        }
+  printf("All the Even Numbers from 1 to %d are \n, n);
+  for(i=2;i<=n;i+=2)
+         {
+           printf("%d \n, i);
+         }
+  return 0;
+ }
